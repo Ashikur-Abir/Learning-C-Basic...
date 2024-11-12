34200c29a358bf4fadf3b9f8a988d179aeeb3ebf
Ashikur Rahman <ashikur016765@gmail.com>
2024-11-12 21:29:42 +0600

    Add files via upload

---
+++ DataType.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+
+int a;
+a =200;
+printf("The int value is %d\n",a);
+
+float b=1.004;
+printf("The foating value is %f\n",b);
+
+long long int Long_value=545546565654;
+printf("long integer value is:-%lld\n",Long_value);
+
+return 0;
+}
