0a846210c14e30c84ab420120b7cbf7999121641
Ashikur Rahman <ashikur016765@gmail.com>
2026-05-14 08:41:15 +0600

    Create Nqueen.c

---
+++ Nqueen.c
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+typedef int index;
+index n;
+index col[100];
+
+bool promising(index i);
+
+void queens(index i)
+{
+    index j;
+
+    if (promising(i))
+        if (i == n)
+        {
+            for (index k = 1; k <= n; k++)
+                cout << col[k] << " ";
+            cout << endl;
+        }
+        else
+            for (j = 1; j <= n; j++) {
+                col[i + 1] = j;
+                queens(i + 1);
+            }
+}
+
+bool promising(index i)
+{
+    index k;
+    bool switch_var;
+
+    k = 1;
+    switch_var = true;
+    while (k < i && switch_var) {
+        if (col[i] == col[k] || abs(col[i] - col[k]) == i - k)
+            switch_var = false;
+        k++;
+    }
+    return switch_var;
+}
+
+int main()
+{
+    cout << "Enter n: ";
+    cin >> n;
+    queens(0);
+    return 0;
+}
